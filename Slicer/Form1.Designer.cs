e34ac580001a5729cb99a9a76a965b36d693e788
João Alves <joao.cpp.sca@gmail.com>
2014-05-16 12:00:10 +0100

    initial commit

---
+++ Slicer/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Slicer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ySlicesNum = new System.Windows.Forms.NumericUpDown();
+            this.xSlicesNum = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.filenameTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ySlicesNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xSlicesNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(614, 383);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(537, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Slice..";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 416);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X Slices:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 417);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y Slices:";
+            // 
+            // ySlicesNum
+            // 
+            this.ySlicesNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ySlicesNum.Location = new System.Drawing.Point(196, 414);
+            this.ySlicesNum.Name = "ySlicesNum";
+            this.ySlicesNum.Size = new System.Drawing.Size(80, 20);
+            this.ySlicesNum.TabIndex = 5;
+            this.ySlicesNum.ValueChanged += new System.EventHandler(this.ySlicesNum_ValueChanged);
+            // 
+            // xSlicesNum
+            // 
+            this.xSlicesNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.xSlicesNum.Location = new System.Drawing.Point(60, 413);
+            this.xSlicesNum.Name = "xSlicesNum";
+            this.xSlicesNum.Size = new System.Drawing.Size(80, 20);
+            this.xSlicesNum.TabIndex = 6;
+            this.xSlicesNum.ValueChanged += new System.EventHandler(this.xSlicesNum_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(439, 442);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Load Image..";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 447);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Filename:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // filenameTxt
+            // 
+            this.filenameTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.filenameTxt.Location = new System.Drawing.Point(64, 443);
+            this.filenameTxt.Name = "filenameTxt";
+            this.filenameTxt.Size = new System.Drawing.Size(133, 20);
+            this.filenameTxt.TabIndex = 9;
+            this.filenameTxt.Text = "output_";
+            this.filenameTxt.TextChanged += new System.EventHandler(this.filenameTxt_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 447);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Format:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "png",
+            "jpg"});
+            this.comboBox1.Location = new System.Drawing.Point(259, 444);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 473);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.filenameTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.xSlicesNum);
+            this.Controls.Add(this.ySlicesNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slicer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ySlicesNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xSlicesNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown ySlicesNum;
+        private System.Windows.Forms.NumericUpDown xSlicesNum;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox filenameTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Slicer/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Slicer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Paint += pictureBox1_Paint;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int wx, wy;
+            int sx, sy;
+            sx = Convert.ToInt32(xSlicesNum.Value) + 1;
+            sy = Convert.ToInt32(ySlicesNum.Value) + 1;
+
+            if (sx > 0)
+                wx = pictureBox1.Width / sx;
+            else
+                wx = pictureBox1.Width;
+
+            if (sy > 0)
+                wy = pictureBox1.Height / sy;
+            else
+                wy = pictureBox1.Height;
+
+
+            var p1 = new Pen(Color.White, 1);
+            p1.DashCap = System.Drawing.Drawing2D.DashCap.Flat;
+            p1.DashPattern = new float[] { 4.0F, 4.0F };
+            var p2 = new Pen(Color.Black, 2);
+            p2.DashCap = System.Drawing.Drawing2D.DashCap.Flat;
+            p2.DashPattern = new float[] { 4.0F, 4.0F };
+
+            for (int x = 0; x < Convert.ToInt32(xSlicesNum.Value); x++)
+            {
+                e.Graphics.DrawLine(p2, new Point((x + 1) * wx, 0), new Point((x + 1) * wx, pictureBox1.Height));
+                e.Graphics.DrawLine(p1, new Point((x + 1) * wx, 0), new Point((x + 1) * wx, pictureBox1.Height));
+
+            }
+            for (int y = 0; y < Convert.ToInt32(ySlicesNum.Value); y++)
+            {
+                e.Graphics.DrawLine(p2, new Point(0, (y + 1) * wy), new Point(pictureBox1.Width, (y + 1) * wy));
+                e.Graphics.DrawLine(p1, new Point(0, (y + 1) * wy), new Point(pictureBox1.Width, (y + 1) * wy));
+            }
+
+            p1.Dispose();
+            p2.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (pictureBox1.Image != null && fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ImageFormat format = ImageFormat.Png;
+                if (comboBox1.SelectedIndex == 0)
+                    format = ImageFormat.Png;
+                else if (comboBox1.SelectedIndex == 1)
+                    format = ImageFormat.Jpeg;
+
+                Image originalImage = pictureBox1.Image;
+                Bitmap originalBitmap = (Bitmap)originalImage;
+                int wx, wy;
+                int sx, sy;
+                sx = Convert.ToInt32(xSlicesNum.Value) + 1;
+                sy = Convert.ToInt32(ySlicesNum.Value) + 1;
+
+                if (sx > 0)
+                    wx = originalImage.Width / sx;
+                else
+                    wx = originalImage.Width;
+
+                if (sy > 0)
+                    wy = originalImage.Height / sy;
+                else
+                    wy = originalImage.Height;
+
+                int count = 0;
+                for (int x = 0; x < sx; x++)
+                {
+                    for (int y = 0; y < sy; y++)
+                    {
+                        Rectangle srcRect = new Rectangle(x * wx, y * wy, wx, wy);
+                        Bitmap cropped = (Bitmap)originalBitmap.Clone(srcRect, originalImage.PixelFormat);
+                       
+                        cropped.Save(fbd.SelectedPath + "\\" + filenameTxt.Text + count + "." + comboBox1.Text, format);
+                        count++;
+                    }
+                }
+
+
+                if (MessageBox.Show("Process Completed on " + fbd.SelectedPath + ". Open Directory?", "Success", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    ProcessStartInfo runExplorer = new ProcessStartInfo();
+                    runExplorer.FileName = "explorer.exe";
+                    runExplorer.Arguments = System.IO.Path.GetDirectoryName(fbd.SelectedPath);
+                    Process.Start(runExplorer);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Load image first.");
+            }
+        }
+
+        private void xSlicesNum_ValueChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void ySlicesNum_ValueChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void filenameTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
